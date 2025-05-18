a2346a2a8d547b336add17aa2f3fcc5ea634320f
CallenHorizon <callen.horizon@gmail.com>
2025-05-18 22:50:04 +0300

    derevo

---
+++ tree.bat
@@ -0,0 +1,31 @@
+md Bocharova
+cd Bocharova
+md Evgenia
+cd Evgenia
+md Ivanovna
+pause
+
+cd ..
+cd..
+cd Bocharova
+echo > 14012003.txt
+cd Evgenia
+cd Ivanovna
+echo > QQK29CFSBII.txt
+pause
+
+cd ..
+cd ..
+cd ..
+del Bocharova /S /Q /F
+pause
+
+
+cd Bocharova
+cd Evgenia
+rd Ivanovna
+cd ..
+rd Evgenia
+cd ..
+rd Bocharova
+pause
